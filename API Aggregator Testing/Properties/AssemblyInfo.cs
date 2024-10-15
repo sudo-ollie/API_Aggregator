e02b7e1f7b4d349f7fb092638a7451d3aa382c64
sudo-ollie <ollikavs@gmail.com>
2024-10-15 15:22:33 +0100

    Establishing Unit Testing File/s

---
+++ API Aggregator Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("API Aggregator Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("API Aggregator Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e4550d3-5cad-4a26-8377-0c941e6fedac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ API Aggregator Testing/ReqMethodTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace API_Aggregator_Testing
+{
+    [TestClass]
+    public class ReqMethodTests
+    {
+        [TestMethod]
+        public void RequestMethod_MethodIsPOST_FunctionDoesNotExecute()
+        {
+            //  'Triple A Test Pattern'
+            //  Example below of how to set up a test, my code needs to be 'function-alised' so that I can actually test it
+
+            //  Arrange
+            //  Creating a version of whatever you're wanting to test could be a request body / cookie / whatever
+            var reservation = new Reservation();
+
+            //  Act
+            //  Testing whatever you're wanting to test - Saving to a var for assertions
+            var result = reservation.MethodToTest();
+
+            //  Assert
+            //  MSTest built in Assert class & methods
+            Assert.IsTrue(result);
+        }
+    }
+}
